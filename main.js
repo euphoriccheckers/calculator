fed4beedd0436d321ab58bc528007dd74a1cd880
euphoriccheckers <98302222+euphoriccheckers@users.noreply.github.com>
2024-11-30 03:32:37 -0700

    Divide function

--- main.js
+++ main.js
@@ -12,3 +12,13 @@ function multiply(num1, num2) {
     console.log(num1 * num2);
     return num1 * num2;
 }
+
+function divide(num1, num2) {
+    if(num2 == 0) {
+        console.log("Can't break the laws of math!");
+        return NaN;
+    } else {
+        console.log(Math.round((num1 / num2) * 100) / 100);
+        return Math.round((num1 / num2) * 100) / 100;
+    }
+}
