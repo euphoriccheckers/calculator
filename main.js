3f66364a0e9cd8993dd0be4c7287d806fac27c50
euphoriccheckers <98302222+euphoriccheckers@users.noreply.github.com>
2024-11-30 03:50:17 -0700

    Operate function and associated variables

--- main.js
+++ main.js
@@ -22,3 +22,31 @@ function divide(num1, num2) {
         return Math.round((num1 / num2) * 100) / 100;
     }
 }
+
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+function operate(num1, operator, num2) {
+    let result = 0;
+
+    switch(operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            console.log("Invalid operator!");
+            return NaN;
+    }
+
+    return result;
+}
