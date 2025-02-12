fcc6a8fca92b4cf775024caf93d2e7f9bd01f4b9
euphoriccheckers <98302222+euphoriccheckers@users.noreply.github.com>
2025-02-12 13:42:34 -0700

    Create basic calculator UI

--- main.js
+++ main.js
@@ -50,3 +50,33 @@ function operate(num1, operator, num2) {
 
     return result;
 }
+
+let display = document.getElementById("display");
+display.innerHTML = `<h1 id='displayText'>0</h1>`;
+let btnBar = document.getElementById("btnBar");
+
+let clrSpan = document.createElement("span");
+clrSpan.id = "clear";
+let clrBtn = document.createElement("button");
+clrBtn.innerHTML = `<span>CLEAR</span>`;
+clrSpan.append(clrBtn);
+btnBar.append(clrSpan);
+
+let delSpan = document.createElement("span");
+delSpan.id = "delete";
+let delBtn = document.createElement("button");
+delBtn.innerHTML = `<span>DELETE</span>`;
+delSpan.append(delBtn);
+btnBar.append(delSpan);
+
+let btnGrid = document.getElementById("btnGrid");
+let btnArray = ["7", "8", "9", String.fromCharCode(247), "4", "5", "6",
+                "x", "1", "2", "3", "-", ".", "0", "=", "+"];
+
+btnArray.forEach(btn => {
+    let button = document.createElement("button");
+    button.innerHTML = `<span>${btn}</span>`;
+    button.id = `${btn}`;
+    button.className = "gridBtn";
+    btnGrid.append(button);
+});
